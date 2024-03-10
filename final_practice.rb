463dc677e79d4a06562bf603be86822ee7694bc7
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-03-10 14:22:27 -0600

    complete 1,2,3

--- final_practice.rb
+++ final_practice.rb
@@ -43,9 +43,18 @@ puts greet_person("Austin", "Andrew", "Carr-Jones")
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+def square (integer)
+integer * integer
+end
+
+puts square(4)
+#bonus
+integer = 5
+puts "the square of #{integer} = #{square(integer)}"
+
+# What is the return value of your method? 16
+# How many arguments did you pass your method? 1
+# What data type was your argument(s)? integer
 
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
@@ -62,3 +71,69 @@ puts greet_person("Austin", "Andrew", "Carr-Jones")
 
 # check_stock(1, "Salsa");
 # => "Salsa - running LOW"
+
+
+# ATTEMPT 1 
+# user enters stock of each item (integer variables for each item). 
+# program check if number is >= directions above, then puts response.
+
+# coffee_stock = (integer1)
+# tortilas_stock = (integer2)
+# cheese_stock = (integer3)
+# salsa_stock = (integer4)
+
+# def check_stock
+#     if 
+#         coffee_stock >= 4
+#         puts "Coffee is stocked"
+#     elsif
+        
+#that attempt wasn't going to work
+# =============================== 
+
+# ATTEMPT 2
+# user enters integer(amount), then string (item)
+
+# stock = (1)
+# ingredient = ("Cheese")
+
+# if ingredient == "Coffee" && stock >= 4
+#     puts "Coffee is stocked"
+#     elsif ingredient == "Coffee" && stock < 4
+#         puts "Coffee needs to be restocked"
+# elsif ingredient == "Tortillas" && stock <= 3
+#         puts "Tortillas - running LOW"
+#      elsif ingredient == "Tortillas" && stock > 10
+#         puts "Tortillas are stocked"
+# elsif ingredient == "Cheese" && stock == 0
+#         puts "Cheese - OUT of stock"
+#     elsif ingredient == "Cheese" && stock >= 1
+#         puts "Cheese is stocked"
+# elsif ingredient == "Salsa" && stock <= 1
+#         puts "Salsa - running LOW"
+# else ingredient == "Salsa" && stock > 5
+#         puts "Salsa is stocked"
+# end
+
+# This if statement was working, but it's more complicated than necessary and it isn't a method
+# Not sure yet how to integrate if statements inside methods.
+
+#=======================
+
+# ATTEMPT 3
+# I have a habit of building if statements by defining ranges instead of leaving the last thing as the catch all "else"
+
+# use the same stock math for all ingredients , they don't need different ranges
+# >=4 == stocked
+# <=3 && > 0 == running low
+# 0 == out of stock
+
+def check_stock(amount, ingredient)
+    if quantity >= 4
+        puts "#{ingredient} - is stocked"
+    elsif quantity > 0
+        puts "#{ingredient} - running LOW"
+    else
+        puts "#{ingredient} - OUT of stock"
+end
+
